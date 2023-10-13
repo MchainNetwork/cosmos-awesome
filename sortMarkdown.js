ab6f9a4365085c809306de5c66dc4e41a82b4d3f
Mchain <marbellachain@gmail.com>
2023-10-13 20:29:10 +0200

    Update sortMarkdown.js

--- sortMarkdown.js
+++ sortMarkdown.js
@@ -18,16 +18,27 @@ collectionSections.sort((a, b) =>
 const collection = '## Collection\n\n' + collectionSections.join('\n');
 
 // Extract the header and TOC section
-const headerAndToc = sections[0].split('## Collection')[0];
+const header = sections[0].split('## Table of Contents')[0];
 
-// Rebuild the markdown with the sorted collection
-const updatedMarkdown =
-  headerAndToc + collection + '## Contributions' + sections[1];
+// Rebuild the markdown without the old TOC
+let updatedMarkdown = header + collection + '## Contributions' + sections[1];
 
-// Update the table of contents
-const updatedTocMarkdown = toc.insert(updatedMarkdown, { maxdepth: 2 });
+// Create a new sorted TOC
+const tocItems = collectionSections
+  .map((section) => {
+    const title = section.split('\n')[0].slice(4);
+    const anchor = title.toLowerCase().replace(/ /g, '-');
+    return `- [${title}](#${anchor})`;
+  })
+  .sort((a, b) => a.localeCompare(b, undefined, { sensitivity: 'base' }));
+
+// Insert the new sorted TOC into the markdown
+updatedMarkdown = updatedMarkdown.replace(
+  header,
+  header + '## Table of Contents\n' + tocItems.join('\n') + '\n'
+);
 
 // Overwrite the original file
-fs.writeFileSync(filePath, updatedTocMarkdown, 'utf8');
+fs.writeFileSync(filePath, updatedMarkdown, 'utf8');
 
 console.log('The file has been sorted and updated.');
